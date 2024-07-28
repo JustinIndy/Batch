5083e54ffcf03c65db0d82c4f2d42645f93d4f16
JustIndy <138534560+JustinIndy@users.noreply.github.com>
2024-07-28 15:09:44 -0400

    Disable Automatic Updates.bat

--- Disable Automatic Updates.bat
+++ Disable Automatic Updates.bat
@@ -1,3 +1,4 @@
+:: By JustIndy
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t "REG_DWORD" /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "AUOptions" /t "REG_DWORD" /d "2" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "ScheduledInstallDay" /t "REG_DWORD" /d "0" /f
